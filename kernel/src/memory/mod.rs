db531f46e8632e04893e394ab2eb57cea1d85584
Viktor Popp <viktorpopp.dev@gmail.com>
2025-03-18 17:38:01 +0100

    Improved paging initialization

--- kernel/src/memory/mod.rs
+++ kernel/src/memory/mod.rs
@@ -1,6 +1,6 @@
 use crate::{boot, trace};
 use spin::once::Once;
-use x86_64::VirtAddr;
+use x86_64::{PhysAddr, VirtAddr, structures::paging::PageTable};
 
 pub mod paging;
 
@@ -11,8 +11,49 @@ pub fn init() {
         PHYS_MEM_OFFSET.call_once(|| VirtAddr::new(hhdm_response.offset()));
     }
     trace!("Hhdm offset: {:#x}\n", phys_mem_offset());
+
+    paging::init();
 }
 
 pub fn phys_mem_offset() -> VirtAddr {
     unsafe { *PHYS_MEM_OFFSET.get_unchecked() }
+}
+
+pub unsafe fn translate_addr(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Option<PhysAddr> {
+    translate_addr_inner(addr, physical_memory_offset)
+}
+
+fn translate_addr_inner(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Option<PhysAddr> {
+    use x86_64::registers::control::Cr3;
+    use x86_64::structures::paging::page_table::FrameError;
+
+    // read the active level 4 frame from the CR3 register
+    let (level_4_table_frame, _) = Cr3::read();
+
+    let table_indexes = [
+        addr.p4_index(),
+        addr.p3_index(),
+        addr.p2_index(),
+        addr.p1_index(),
+    ];
+    let mut frame = level_4_table_frame;
+
+    // traverse the multi-level page table
+    for &index in &table_indexes {
+        // convert the frame into a page table reference
+        let virt = physical_memory_offset + frame.start_address().as_u64();
+        let table_ptr: *const PageTable = virt.as_ptr();
+        let table = unsafe { &*table_ptr };
+
+        // read the page table entry and update `frame`
+        let entry = &table[index];
+        frame = match entry.frame() {
+            Ok(frame) => frame,
+            Err(FrameError::FrameNotPresent) => return None,
+            Err(FrameError::HugeFrame) => panic!("huge pages not supported"),
+        };
+    }
+
+    // calculate the physical address by adding the page offset
+    Some(frame.start_address() + u64::from(addr.page_offset()))
 }
--- kernel/src/memory/paging.rs
+++ kernel/src/memory/paging.rs
@@ -1,14 +1,20 @@
 use crate::{error, hlt_loop, print};
 use x86_64::structures::idt::{InterruptStackFrame, PageFaultErrorCode};
 use x86_64::{VirtAddr, structures::paging::PageTable};
+use x86_64::structures::paging::OffsetPageTable;
 
-pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
+pub fn init() -> OffsetPageTable<'static> {
+    let level_4_table = unsafe { active_level_4_table(super::phys_mem_offset()) };
+    unsafe { OffsetPageTable::new(level_4_table, super::phys_mem_offset()) }
+}
+
+pub unsafe fn active_level_4_table(_physical_memory_offset: VirtAddr) -> &'static mut PageTable {
     use x86_64::registers::control::Cr3;
 
     let (level_4_table_frame, _) = Cr3::read();
 
     let phys = level_4_table_frame.start_address();
-    let virt = physical_memory_offset + phys.as_u64();
+    let virt = super::phys_mem_offset() + phys.as_u64();
     let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
 
     unsafe { &mut *page_table_ptr }

a068a9da2dc4fe461253d00fe63d83495a83a770
Viktor Popp <ViktorPopp.Dev@gmail.com>
2025-04-03 17:25:48 +0200

    Improved keyboard drivers output

--- kernel/src/devices/keyboard/mod.rs
+++ kernel/src/devices/keyboard/mod.rs
@@ -1,15 +1,32 @@
 use crate::trace;
 use x86_64::structures::idt::InterruptStackFrame;
 use crate::interrupts::InterruptIndex;
-use x86_64::instructions::port::Port;
+use lazy_static::lazy_static;
 
 pub extern "x86-interrupt" fn interrupt_handler(_stack_frame: InterruptStackFrame) {
-    let mut keyboard_port: Port<u8> = Port::new(0x60);
-    
-    // Read the scancode from the keyboard data port
-    let scancode: u8 = unsafe { keyboard_port.read() };
-    
-    trace!("Received keyboard interrupt with scancode: {:#x}\n", scancode);
+    use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
+    use spin::Mutex;
+    use x86_64::instructions::port::Port;
+
+    lazy_static! {
+        static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> =
+            Mutex::new(Keyboard::new(ScancodeSet1::new(),
+                layouts::Us104Key, HandleControl::Ignore)
+            );
+    }
+
+    let mut keyboard = KEYBOARD.lock();
+    let mut port = Port::new(0x60);
+
+    let scancode: u8 = unsafe { port.read() };
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => trace!("Received keyboard interrupt with key: {}\n", character),
+                DecodedKey::RawKey(key) => trace!("Received keyboard interrupt with raw key:{:?}\n", key),
+            }
+        }
+    }
 
     unsafe {
         crate::interrupts::PICS.lock()

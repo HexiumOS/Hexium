1f15e041475a164085252662ad8bb2d80c09a353
Viktor Popp <ViktorPopp.Dev@gmail.com>
2025-04-04 15:51:41 +0200

    Added QOL improvements

--- kernel/src/devices/keyboard/mod.rs
+++ kernel/src/devices/keyboard/mod.rs
@@ -1,18 +1,20 @@
-use crate::trace;
-use x86_64::structures::idt::InterruptStackFrame;
 use crate::interrupts::InterruptIndex;
+use crate::trace;
 use lazy_static::lazy_static;
+use x86_64::structures::idt::InterruptStackFrame;
 
 pub extern "x86-interrupt" fn interrupt_handler(_stack_frame: InterruptStackFrame) {
-    use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
+    use pc_keyboard::{DecodedKey, HandleControl, Keyboard, ScancodeSet1, layouts};
     use spin::Mutex;
     use x86_64::instructions::port::Port;
 
     lazy_static! {
         static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> =
-            Mutex::new(Keyboard::new(ScancodeSet1::new(),
-                layouts::Us104Key, HandleControl::Ignore)
-            );
+            Mutex::new(Keyboard::new(
+                ScancodeSet1::new(),
+                layouts::Us104Key,
+                HandleControl::Ignore
+            ));
     }
 
     let mut keyboard = KEYBOARD.lock();
@@ -22,14 +24,19 @@ pub extern "x86-interrupt" fn interrupt_handler(_stack_frame: InterruptStackFram
     if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
         if let Some(key) = keyboard.process_keyevent(key_event) {
             match key {
-                DecodedKey::Unicode(character) => trace!("Received keyboard interrupt with key: {}\n", character),
-                DecodedKey::RawKey(key) => trace!("Received keyboard interrupt with raw key:{:?}\n", key),
+                DecodedKey::Unicode(character) => {
+                    trace!("Received keyboard interrupt with key: {}\n", character)
+                }
+                DecodedKey::RawKey(key) => {
+                    trace!("Received keyboard interrupt with key: {:?}\n", key)
+                }
             }
         }
     }
 
     unsafe {
-        crate::interrupts::PICS.lock()
+        crate::interrupts::PICS
+            .lock()
             .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
     }
 }

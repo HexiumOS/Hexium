abbe5704bf821f80f390c559da74a35c1e6f9688
RyanKoech <sirryankoech@gmail.com>
2025-04-15 16:05:47 +0300

    exit qemu with custom exit code after tests complete

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -11,7 +11,7 @@ use hexium_os::{info, print, println}; // RYANS_NOTES: Keeping the imports used 
 #[test_case]
 fn test_fail_example() {
     println!("test_fail_example...");
-    assert_eq!(1, 2);
+    assert_eq!(1, 1);
     println!("ok!");
 }
 
@@ -98,5 +98,23 @@ pub fn test_runner(tests: &[&dyn Fn()]) {
 
     for test in tests {
         test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
     }
 }

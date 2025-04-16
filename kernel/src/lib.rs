0349c99e2daff66ae0b727ccbface4a8068aed91
RyanKoech <sirryankoech@gmail.com>
2025-04-16 09:41:38 +0300

    define testable trait for tests

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -157,6 +157,20 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
     }
 }
 
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where T : Fn(),
+{
+    fn run(&self) -> () {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }   
+}
+
 // #[panic_handler]
 // fn panic(_info: &PanicInfo) -> ! {
 //     loop {}

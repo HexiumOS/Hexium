0248de8875d2533f27085c288682612268a914f2
RyanKoech <sirryankoech@gmail.com>
2025-04-17 12:29:50 +0300

    disable test harness

--- kernel/tests/should_panic.rs
+++ kernel/tests/should_panic.rs
@@ -1,8 +1,5 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
-#![reexport_test_harness_main="test_main"]
 
 use core::panic::PanicInfo;
 
@@ -11,7 +8,9 @@ use hexium_os::{exit_qemu, init, serial_print, serial_println};
 #[unsafe(no_mangle)]
 unsafe extern "C" fn kmain() -> ! {
   init();
-  test_main();
+  should_fail();
+  serial_print!("[test did not panic]");
+  exit_qemu(hexium_os::QemuExitCode::Failed);
   loop{}
 }
 
@@ -22,18 +21,6 @@ fn panic(_info: &PanicInfo) -> ! {
   loop {}
 }
 
-pub fn test_runner(tests: &[&dyn Fn()]) {
-  serial_println!("Running {} tests", tests.len());
-
-  for test in tests {
-    test();
-    serial_print!("[test did not panic]");
-    exit_qemu(hexium_os::QemuExitCode::Failed);
-  }
-  exit_qemu(hexium_os::QemuExitCode::Success);
-}
-
-#[test_case]
 fn should_fail() {
   serial_println!("should_panic::should_fail...\t");
   assert_eq!(0,1);

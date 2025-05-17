2e007089e1d044aa0be918cbb44650d8d7708c72
Viktor Popp <ViktorPopp.Dev@gmail.com>
2025-05-17 12:36:33 +0200

    Can now translate addrs

--- kernel/src/arch/x86_64/memory/paging.rs
+++ kernel/src/arch/x86_64/memory/paging.rs
@@ -1,4 +1,4 @@
-use x86_64c::structures::paging::PageTable;
+use x86_64c::{PhysAddr, VirtAddr, structures::paging::PageTable};
 
 /// Returns a mutable reference to the active level 4 table.
 pub unsafe fn active_level_4_table() -> &'static mut PageTable {
@@ -11,4 +11,45 @@ pub unsafe fn active_level_4_table() -> &'static mut PageTable {
     let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
 
     unsafe { &mut *page_table_ptr }
+}
+
+/// Translates the given virtual address to the mapped physical address, or
+/// `None` if the address is not mapped.
+pub unsafe fn translate_addr(addr: VirtAddr) -> Option<PhysAddr> {
+    translate_addr_inner(addr)
+}
+
+/// Private function that is called by `translate_addr`.
+fn translate_addr_inner(addr: VirtAddr) -> Option<PhysAddr> {
+    use x86_64c::registers::control::Cr3;
+    use x86_64c::structures::paging::page_table::FrameError;
+
+    let (level_4_table_frame, _) = Cr3::read();
+
+    let table_indexes = [
+        addr.p4_index(),
+        addr.p3_index(),
+        addr.p2_index(),
+        addr.p1_index(),
+    ];
+    let mut frame = level_4_table_frame;
+
+    // traverse the multi-level page table
+    for &index in &table_indexes {
+        // convert the frame into a page table reference
+        let virt = super::hhdm_offset() + frame.start_address().as_u64();
+        let table_ptr: *const PageTable = virt.as_ptr();
+        let table = unsafe { &*table_ptr };
+
+        // read the page table entry and update `frame`
+        let entry = &table[index];
+        frame = match entry.frame() {
+            Ok(frame) => frame,
+            Err(FrameError::FrameNotPresent) => return None,
+            Err(FrameError::HugeFrame) => panic!("huge pages not supported"),
+        };
+    }
+
+    // calculate the physical address by adding the page offset
+    Some(frame.start_address() + u64::from(addr.page_offset()))
 }

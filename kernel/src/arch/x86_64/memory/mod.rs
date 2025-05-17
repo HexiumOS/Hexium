6fedff2a11b1824b536d85d35d2ddddad8cd1f4f
Viktor Popp <ViktorPopp.Dev@gmail.com>
2025-05-17 12:55:43 +0200

    Added support for offset page tables

--- kernel/src/arch/x86_64/memory/mod.rs
+++ kernel/src/arch/x86_64/memory/mod.rs
@@ -1,20 +1,28 @@
 use crate::trace;
 use spin::Once;
-use x86_64c::VirtAddr;
+use x86_64c::{VirtAddr, structures::paging::OffsetPageTable};
 
 pub mod paging;
 pub mod pmm;
 pub mod vmm;
 
-static PHYS_MEM_OFFSET: Once<VirtAddr> = Once::new();
+static mut MAPPER: Once<OffsetPageTable<'static>> = Once::new();
+static HHDM_OFFSET: Once<VirtAddr> = Once::new();
 
 pub fn init() {
     if let Some(hhdm_response) = crate::boot::HHDM_REQUEST.get_response() {
-        PHYS_MEM_OFFSET.call_once(|| VirtAddr::new(hhdm_response.offset()));
+        HHDM_OFFSET.call_once(|| VirtAddr::new(hhdm_response.offset()));
     }
     trace!("Hhdm offset: {:#x}", hhdm_offset());
 }
 
 pub fn hhdm_offset() -> VirtAddr {
-    unsafe { *PHYS_MEM_OFFSET.get_unchecked() }
+    unsafe { *HHDM_OFFSET.get_unchecked() }
+}
+
+pub fn mapper() -> &'static OffsetPageTable<'static> {
+    #[allow(static_mut_refs)]
+    unsafe {
+        MAPPER.get_unchecked()
+    }
 }
--- kernel/src/arch/x86_64/memory/vmm.rs
+++ kernel/src/arch/x86_64/memory/vmm.rs
@@ -1,5 +1,8 @@
 use crate::{error, hlt_loop, print};
-use x86_64c::structures::idt::{InterruptStackFrame, PageFaultErrorCode};
+use x86_64c::structures::{
+    idt::{InterruptStackFrame, PageFaultErrorCode},
+    paging::OffsetPageTable,
+};
 
 pub extern "x86-interrupt" fn page_fault_handler(
     stack_frame: InterruptStackFrame,
@@ -12,4 +15,11 @@ pub extern "x86-interrupt" fn page_fault_handler(
     error!("Error Code: {:?}", error_code);
     print!("\n{:#?}\n", stack_frame);
     hlt_loop();
+}
+
+pub fn initialize_offset_table() -> OffsetPageTable<'static> {
+    unsafe {
+        let level_4_table = super::paging::active_level_4_table();
+        OffsetPageTable::new(level_4_table, super::hhdm_offset())
+    }
 }

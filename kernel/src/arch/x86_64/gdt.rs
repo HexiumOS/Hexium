0b99d8904b7deed6accc3be39d6efea7938010c6
Viktor Popp <viktorpopp.dev@gmail.com>
2025-07-16 18:42:07 +0200

    temp commit

--- kernel/src/arch/x86_64/gdt.rs
+++ kernel/src/arch/x86_64/gdt.rs
@@ -1,4 +1,17 @@
+use crate::arch::x86_64::registers::segmentation::{CS, SS, SegmentSelector};
+
+use super::PrivilegeLevel;
+use bitflags::bitflags;
 use lazy_static::lazy_static;
+
+pub fn init() {
+    GDT.0.load();
+    unsafe {
+        CS::set_reg(GDT.1.code_selector);
+        SS::set_reg(GDT.1.data_selector);
+        load_tss(GDT.1.tss_selector);
+    }
+}
 
 lazy_static! {
     static ref GDT: (GlobalDescriptorTable, Selectors) = {
@@ -40,11 +53,79 @@ struct Selectors {
     data_selector: SegmentSelector,
 }
 
-pub fn init() {
-    GDT.0.load();
-    unsafe {
-        CS::set_reg(GDT.1.code_selector);
-        SS::set_reg(GDT.1.data_selector);
-        load_tss(GDT.1.tss_selector);
+pub enum Descriptor {
+    UserSegment(u64),
+    SystemSegment(u64, u64),
+}
+
+impl Descriptor {
+    #[inline]
+    pub fn dpl(self) -> PrivilegeLevel {
+        let value_low = match self {
+            Descriptor::UserSegment(v) => v,
+            Descriptor::SystemSegment(v, _) => v,
+        };
+        let dpl = (value_low & DescriptorFlags::DPL_RING_3.bits()) >> 45;
+        PrivilegeLevel::from_u16(dpl as u16)
     }
+
+    #[inline]
+    pub const fn kernel_code_segment() -> Descriptor {
+        Descriptor::UserSegment(DescriptorFlags::KERNEL_CODE64.bits())
+    }
+
+    #[inline]
+    pub const fn kernel_data_segment() -> Descriptor {
+        Descriptor::UserSegment(DescriptorFlags::KERNEL_DATA.bits())
+    }
+}
+
+bitflags! {
+    /// Flags for a GDT descriptor. Not all flags are valid for all descriptor types.
+    #[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Debug, Clone, Copy)]
+    pub struct DescriptorFlags: u64 {
+        const ACCESSED          = 1 << 40;
+        const WRITABLE          = 1 << 41;
+        const CONFORMING        = 1 << 42;
+        const EXECUTABLE        = 1 << 43;
+        const USER_SEGMENT      = 1 << 44;
+        const DPL_RING_3        = 3 << 45;
+        const PRESENT           = 1 << 47;
+        const AVAILABLE         = 1 << 52;
+        const LONG_MODE         = 1 << 53;
+        const DEFAULT_SIZE      = 1 << 54;
+        const GRANULARITY       = 1 << 55;
+        const LIMIT_0_15        = 0xFFFF;
+        const LIMIT_16_19       = 0xF << 48;
+        const BASE_0_23         = 0xFF_FFFF << 16;
+        const BASE_24_31        = 0xFF << 56;
+    }
+}
+
+impl DescriptorFlags {
+    const COMMON: Self = Self::from_bits_truncate(
+        Self::USER_SEGMENT.bits()
+            | Self::PRESENT.bits()
+            | Self::WRITABLE.bits()
+            | Self::ACCESSED.bits()
+            | Self::LIMIT_0_15.bits()
+            | Self::LIMIT_16_19.bits()
+            | Self::GRANULARITY.bits(),
+    );
+    pub const KERNEL_DATA: Self =
+        Self::from_bits_truncate(Self::COMMON.bits() | Self::DEFAULT_SIZE.bits());
+    pub const KERNEL_CODE32: Self = Self::from_bits_truncate(
+        Self::COMMON.bits() | Self::EXECUTABLE.bits() | Self::DEFAULT_SIZE.bits(),
+    );
+    pub const KERNEL_CODE64: Self = Self::from_bits_truncate(
+        Self::COMMON.bits() | Self::EXECUTABLE.bits() | Self::LONG_MODE.bits(),
+    );
+    pub const USER_DATA: Self =
+        Self::from_bits_truncate(Self::KERNEL_DATA.bits() | Self::DPL_RING_3.bits());
+    /// A flat 32-bit user code segment
+    pub const USER_CODE32: Self =
+        Self::from_bits_truncate(Self::KERNEL_CODE32.bits() | Self::DPL_RING_3.bits());
+    /// A 64-bit user code segment
+    pub const USER_CODE64: Self =
+        Self::from_bits_truncate(Self::KERNEL_CODE64.bits() | Self::DPL_RING_3.bits());
 }
--- kernel/src/arch/x86_64/mod.rs
+++ kernel/src/arch/x86_64/mod.rs
@@ -1,8 +1,32 @@
+pub mod addr;
 pub mod boot;
 pub mod gdt;
+pub mod registers;
 pub mod writer;
 
 pub fn init() {
     assert!(boot::BASE_REVISION.is_supported());
     writer::init();
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+#[repr(u8)]
+pub enum PrivilegeLevel {
+    Ring0 = 0,
+    Ring1 = 1,
+    Ring2 = 2,
+    Ring3 = 3,
+}
+
+impl PrivilegeLevel {
+    #[inline]
+    pub const fn from_u16(value: u16) -> PrivilegeLevel {
+        match value {
+            0 => PrivilegeLevel::Ring0,
+            1 => PrivilegeLevel::Ring1,
+            2 => PrivilegeLevel::Ring2,
+            3 => PrivilegeLevel::Ring3,
+            _ => panic!("invalid privilege level"),
+        }
+    }
 }

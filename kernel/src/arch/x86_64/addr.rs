0baa6effda83e4c066adcf672becb19e28cb1ff0
Viktor Popp <viktorpopp.dev@gmail.com>
2025-07-16 18:42:59 +0200

    Revert "temp commit"
    This reverts commit eeb4561f6cd8fb99ee9b46ee5d41902e0819805e.

--- kernel/src/arch/x86_64/addr.rs
+++
@@ -1,7 +0,0 @@
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-#[repr(transparent)]
-pub struct VirtAddr(u64);
-
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-#[repr(transparent)]
-pub struct PhysAddr(u64);

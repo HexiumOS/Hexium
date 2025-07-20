b78b58e16b65332b3f308bef342e250b49e0951e
Viktor Popp <viktorpopp.dev@gmail.com>
2025-07-20 17:09:16 +0200

    I am dumb

---
+++ kernel/src/hal/mod.rs
@@ -0,0 +1,14 @@
+pub fn init() {
+    crate::arch::init();
+}
+
+pub fn halt_device() -> ! {
+    unsafe {
+        core::arch::asm!("cli");
+    }
+    loop {
+        unsafe {
+            core::arch::asm!("hlt");
+        }
+    }
+}
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -3,21 +3,11 @@
 
 #[unsafe(no_mangle)]
 unsafe extern "C" fn kmain() -> ! {
-    halt_device();
+    hexium::hal::init();
+    hexium::hal::halt_device();
 }
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
-    halt_device();
-}
-
-fn halt_device() -> ! {
-    unsafe {
-        core::arch::asm!("cli");
-    }
-    loop {
-        unsafe {
-            core::arch::asm!("hlt");
-        }
-    }
+    hexium::hal::halt_device();
 }
